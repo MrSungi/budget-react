10a58ba2aea5180f9026bbaa6dbf33d1bb9ca42f
MrSungi <ignas.sungaila@gmail.com>
2021-11-18 23:04:37 +0200

    some data imported

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {Container} from 'semantic-ui-react';
 import './App.css';
 import MainHeader from './components/MainHeader';
@@ -7,6 +8,9 @@ import DisplayBalances from './components/DisplayBalances';
 import EntryLine from './components/EntryLine';
 
 function App() {
+  // eslint-disable-next-line
+  const [entries, setEntries] = useState(initialEntries)
+
   return (
     <Container>
 
@@ -17,8 +21,14 @@ function App() {
 
     <MainHeader title='History' type='h3' />
 
-    <EntryLine description='income' value='$10.00' />
-    <EntryLine description='expense' value='$10.00' isExpense/>
+
+    {entries.map((entry) => (
+      <EntryLine
+        description={entry.description}
+        value={entry.value}
+        isExpense={entry.isExpense}
+      />
+    ))}
 
     <MainHeader title='Add new transaction' type='h3' />
     <NewEntryForm />
@@ -27,3 +37,26 @@ function App() {
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    description : 'Work income',
+    value : '$1000.00',
+    isExpense : false
+  },
+  {
+    description : 'Water bill',
+    value : '$20.00',
+    isExpense : true
+  },
+  {
+    description : 'Rent',
+    value : '$300',
+    isExpense : true
+  },
+  {
+    description : 'Power bill',
+    value : '$50.00',
+    isExpense : true
+  }
+]
